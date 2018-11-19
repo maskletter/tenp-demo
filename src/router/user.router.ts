fd9ff07c0f011dd420c057038f91a4fbf9cf1155
maskletter <504840815@qq.com>
2018-11-19 16:46:33 +0800

    更新demo版本库，及代码目录整理

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -3,6 +3,8 @@ import * as Kvl from 'kvl';
 import { Router, config } from 'kvl';
 import { Login_Validation, UserInfo_Validation } from '../validation/user.validation'
 import { UserInterceptor } from '../interceptor/user.interceptor'
+import LoginAchieve from '../achieve/user/login.achieve'
+import UserinfoAchieve from '../achieve/user/userinfo.achieve'
 
 
 @Router({
@@ -13,29 +15,12 @@ export default class UserRouter{
 
 	@config({ url: '/login', name: '用户登录', type: 'get', validation: Login_Validation, interceptorLevel: 3 })
 	private login(req: Kvl.Request, res: Kvl.Response): void {
-
-		(req as any).session.userId = 113;
-		res.json({
-			code: 1,
-			msg: '登录成功',
-			param: req.query
-		})
+		LoginAchieve(req, res);
 	}
 
 	@config({ url: '/userinfo', name: '获取用户信息', type: ['get','post'] })
 	private userInfo(req: Kvl.Request, res: Kvl.Response): void {
-
-		res.json({
-			code: 1,
-			msg: '获取用户信息成功',
-			user: {
-				nickname: 'maskletter',
-				gender: 'male',
-				email: 'maskletter@outlook.com',
-				age: 21
-			}
-		})
-		
+		UserinfoAchieve(req, res);
 	}
 
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 
 import * as Kvl from 'kvl';
-import { Router, config, ValidationDone } from 'kvl';
+import { MainKvl ,Router, config, ValidationDone } from 'kvl';
 import UserRouter from './router/user.router'
 import HomeClass from './router/main.router'
 import * as session from 'express-session';
+import * as path from 'path';
 
 
 module Main {
@@ -24,17 +25,17 @@ module Main {
 		private msg: string = 'Hello, world'
 
 		@config({ url: '/hello', name: 'hello', type: 'get' })
-		private hello(req, res): void {
+		private hello(req: Kvl.Request, res: Kvl.Response): void {
 			res.end(`<h1>${this.msg}</h1>`)
 		}
 
 	}
 
 
-	const kvlInit = Kvl({
+	const kvlInit = MainKvl({
 		port: 8080,
 		router: [ UserRouter, HelloWord ],
-		this: true,
+		useThis: true,
 		static: process.cwd()+'/static',
 		use: [
 			session({
@@ -42,10 +43,13 @@ module Main {
 			  resave: false,
 			  saveUninitialized: true
 			})
-		]
+		],
+		throw(request: Kvl.Request, response: Kvl.Response, status: number, error: Error){
+			response.status(status).sendFile(path.resolve(__dirname,'../assets/error/error.html'));
+		}
 	})
 	 
-	require('child_process').exec('start http://localhost:8080')
+	// require('child_process').exec('start http://localhost:8080')
 	console.log('服务启动完成')
 
 }
